0f57fe7e99017444a1a39f5b3948c8585ffb68af
Sahil390 <snarang445566@gmail.com>
2023-09-29 22:10:29 +0530

    Created a palindrom program.

---
+++ sahil.119480/palindrom.c
@@ -0,0 +1,28 @@
+// Implement a function to determine if a given string is a palindrome.
+
+#include<stdio.h>
+
+int main()
+{
+    char str[100];
+    printf("Enter a string : ");
+    scanf("%s",str);
+    int i,j;
+    for(i = 0;str[i] != '\0';i++);
+    for(j = 0;j<i/2;j++)
+    {
+        if(str[j] != str[i-j-1])
+        {
+            printf("This is not a palindrome.\n");
+            break;
+        }
+        else if(j == i/2-1)
+        {
+            printf("This is a palindrome.\n");
+        }
+        else
+        {
+            continue;
+        }
+    }
+}
