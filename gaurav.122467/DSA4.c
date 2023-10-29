043f43e366be6a0c55266a4d291ad747915435c6
GauravSrivastava-prog <Gaurav.122467@stu.upes.ac.in>
2023-10-29 13:06:12 +0530

    Modified Selection

--- gaurav.122467/DSA4.c
+++ gaurav.122467/DSA4.c
@@ -4,7 +4,7 @@ void sort(int a[],int size)
     int temp;
     for(int i=0;i<size;i++)
     {
-        for(int j=i;j<size;j++)
+        for(int j=i+1;j<size;j++)
         {
             if(a[i]>a[j])
             {
